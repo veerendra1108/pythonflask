97029f971f3effe10f19b172171978e855c2c4d5
MUKESH PATNEEDI <veerendra.patineedi@gmail.com>
2020-06-22 22:09:31 +0530

    bug fix

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def main():
 
 @app.route('/how are you')
 def hello():
-    return 'I am good, hoo about yo?'
+    return 'I am good, hoo about you?'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
